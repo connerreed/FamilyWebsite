7f734b03f83f96d18e995da7e50a0487df25959d
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-06-02 21:34:30 -0500

    Frontend started
    Navbar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import CustomNavbar from "./CustomNavbar";
+import Home from "./Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. This is some added test text.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+            <div className="App">
+                <CustomNavbar />
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </div>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
