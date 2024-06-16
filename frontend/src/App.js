3c1dc839b6e3e79ffabeb6ec1b7a7fc8685adb8f
Conner Reed <98499861+connerreed@users.noreply.github.com>
2024-06-16 16:23:22 -0500

    Added pages + page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import "./css/App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import CustomNavbar from "./components/CustomNavbar";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Pictures from "./components/Pictures";
+import Recipes from "./components/Recipes";
+import Videos from "./components/Videos";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -13,6 +17,10 @@ function App() {
                 <div className="content">
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="/recipes" element={<Recipes />} />
+                        <Route path="/pictures" element={<Pictures />} />
+                        <Route path="/videos" element={<Videos />} />
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </div>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 const Home = () => {
 
     return (
         <div>
-            Home
+            <h1>Home</h1>
         </div>
     );
 };
